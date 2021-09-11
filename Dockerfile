7073f494922245ebfdeaf7e4c9d4cb927317c026
dezordi <zimmer.filipe@gmail.com>
2021-09-11 18:41:22 -0300

    Update to version 0.0.5 - improve consensus assembly of low quality mapping regions - insertion of exact coverage percentage based on depth treshold - create arguments to modify depth treshold of intrahost minor alleles and to trimm reads.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM dezordi/iam_sarscov2:0.0.4
+FROM dezordi/iam_sarscov2:0.0.5
 
 ENV REFERENCE='' \
     FASTQ1='' \
@@ -7,10 +7,12 @@ ENV REFERENCE='' \
     THREADS='' \
     DEPTH='' \
     MIN_LEN='' \
-    ADAPTERS='' 
+    ADAPTERS='' \
+    DP_INTRAHOST='' \
+    TRIMM_LEN=''
 
 RUN /bin/bash -c "source /home/pango_update"
 
 COPY $REFERENCE $FASTQ1 $FASTQ2 $ADAPTERS /home/
 
-ENTRYPOINT /bin/bash -c "source /home/sars2_assembly_docker $REFERENCE $FASTQ1 $FASTQ2 $PREFIXOUT $THREADS $DEPTH $MIN_LEN $ADAPTERS"
+ENTRYPOINT /bin/bash -c "source /home/sars2_assembly_docker $REFERENCE $FASTQ1 $FASTQ2 $PREFIXOUT $THREADS $DEPTH $MIN_LEN $ADAPTERS $DP_INTRAHOST $TRIMM_LEN"
--- python_scripts/get_mvs.py
+++ python_scripts/get_mvs.py
@@ -5,6 +5,7 @@ parser.add_argument("-f", "--fasta", help="fasta_file", required=True)
 parser.add_argument("-pr", "--prefixout", help="prefixout", required=True)
 parser.add_argument("-dp", "--depth", help="depth", required=True)
 parser.add_argument("-p", "--threads", help="threads", required=True)
+parser.add_argument("-di", "--intrahost_depth", help="minimum intrahost depth", required=True)
 
 #Storing argument on variables
 args = parser.parse_args()
@@ -12,10 +13,11 @@ fasta_file = args.fasta
 prefixout = args.prefixout
 depth = args.depth
 threads = args.threads
+intrahost_depth = args.intrahost_depth
 
 try:
     with open(f"{prefixout}.depth{depth}.fa.bc","w") as bamread_output: #abm = annotation bed merged
-        bwa_read = (f"bam-readcount -d 50000 -b 30 -q 30 -w 0 -f {fasta_file} {prefixout}.sorted.bam")
+        bwa_read = (f"bam-readcount -d 50000 -q 30 -w 0 -f {fasta_file} {prefixout}.sorted.bam")
         bwa_read = shlex.split(bwa_read)
         cmd_bwa_read = subprocess.Popen(bwa_read, stdout=bamread_output)
         cmd_bwa_read.wait()
@@ -24,6 +26,6 @@ try:
         mafft = shlex.split(mafft)
         cmd_mafft = subprocess.Popen(mafft,stdout=mafft_output)
         cmd_mafft.wait()
-    os.system(f"python /home/python_scripts/intrahost.py -in {prefixout}.depth{depth}.fa.bc -al {prefixout}.depth{depth}.fa.algn")
+    os.system(f"python /home/python_scripts/intrahost.py -in {prefixout}.depth{depth}.fa.bc -al {prefixout}.depth{depth}.fa.algn -dp {intrahost_depth}")
 except:
     print("Error in minor variants step")
