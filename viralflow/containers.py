6f5e860129ee0200c7baf4f7a7029a0ffbe60d13
AMarinhoSN <amarinhosn@pm.me>
2021-10-06 16:50:30 -0300

    [add] run singularity container function

--- viralflow/containers.py
+++ viralflow/containers.py
@@ -1,5 +1,5 @@
 import os
-
+import subprocess
 __author__ = "Antonio Marinho da Silva Neto"
 __copyright__ = "Copyright 2021, Rede Genomica Fiocruz"
 __license__ = "GPL"
@@ -61,3 +61,73 @@ def buildSing(output_dir, singfl_path, container_name='viralflow_container',
     cmd_str = sing_path+' build '+sing_opt+' ' + output_dir+container_name
     cmd_str += ' '+singfl_path
     os.system(cmd_str)
+
+
+def run_sing_container(container_img, input_dir, ref_gnm, fastq_R1, fastq_R2,
+                       prefix_out, adapters_file, threads=1, depth=5,
+                       min_len=75, min_dp_intrahost=100, trim_len=0,
+                       sing_call='singularity', dry=False):
+    '''
+    run singularity viralflow singularity container on a pair of fastq files
+
+    Parameters
+    ----------
+    container_img:<path>
+        Path to viralflow singularity container
+
+    input_dir:<path>
+        Path for directory containing input files
+
+    ref_gnm:<path>
+        Path for reference genome (must be relative to input_dir)
+
+    fastq_R1:<path>
+        Path for fastq R1 filename (must be relative to input_dir)
+
+    fastq_R2:<path>
+        Path for fastq R2 filename (must be relative to input_dir)
+
+    prefix_out:<str>
+        Prefix for output filenames (should not have '/')
+
+    adapters_file:<path>
+        Path for adapters (primers) fasta file (must be relative to input dir)
+
+    threads:<int>
+        Number of threads to use on the sample process (default=1)
+
+    depth:<int>
+        Minimum depth to mask unanssembled regions
+
+    min_len:<int>
+        Minimum length to trimm sequences
+
+    min_dp_intrahost:<int>
+        Minimum depth value to consider intrahost minor allele (default = 100)
+
+    trim_len:<int>
+        Length to trimm front and tail of reads on fastp analysis (default = 0)
+
+    sing_call:<str>
+        string to call singularity, must be absolute path or 'singularity'
+        (default = 'singularity')
+    '''
+    # get command line
+    cmd = sing_call+' run --bind '
+    cmd += input_dir+':/data/ '
+    cmd += '--env REFERENCE='+ref_gnm+' '
+    cmd += '--env FASTQ1='+fastq_R1+' '
+    cmd += '--env FASTQ2='+fastq_R2+' '
+    cmd += '--env PREFIXOUT='+prefix_out+' '
+    cmd += '--env THREADS='+str(threads)+' '
+    cmd += '--env DEPTH='+str(depth)+' '
+    cmd += '--env MIN_LEN='+str(min_len)+' '
+    cmd += '--env ADAPTERS='+adapters_file+' '
+    cmd += '--env DP_INTRAHOST='+str(min_dp_intrahost)+' '
+    cmd += '--env TRIMM_LEN='+str(trim_len)+' '
+    cmd += '--writable-tmpfs '+container_img
+    # run command
+    if dry is True:
+        print(cmd)
+        return None
+    subprocess.run(cmd, shell=True, check=True)
