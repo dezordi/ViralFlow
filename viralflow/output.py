187385e42a59a66ced00018e70ab728535e4b060
AMarinhoSN <amarinhosn@pm.me>
2021-11-05 18:29:37 -0300

    [add] output handling module

---
+++ viralflow/output.py
@@ -0,0 +1,188 @@
+import os
+import pandas as pd
+
+# get list of results dir
+def get_consensus_seq(fasta_path, cod):
+    '''
+    get sequence line from a single sequence fasta file
+    '''
+    with open(fasta_path, 'r') as fasta_fl:
+        for line in fasta_fl:
+            if line.startswith('>'):
+                # sanity check
+                assert(cod in line)
+            else:
+                return line
+
+
+def get_mut(row):
+    return row['REF']+str(row['POS'])+row['ALT']
+
+
+def __parse_xsv(cod, xsv_path, sep=','):
+    '''
+    get dataframe from tabular data (ex: csv and tsv)
+    '''
+    df = pd.read_csv(xsv_path, sep=sep)
+    df['cod'] = [cod for i in range(0, len(df))]
+    return df
+
+
+def __check_if_outfls_exist(cod, out_fls_lst, mut_fl):
+    '''
+    Check if expected files exist
+    '''
+    err_dct_lst = []
+    for fl in out_fls_lst:
+        try:
+            assert(os.path.isfile(fl))
+        except(AssertionError):
+            srvc = fl.split('.')[-2]
+            if fl == mut_fl:
+                srvc = 'Mutations'
+            print(f'  :: WARNING: missing {srvc} output for '+cod)
+            type = 'ERROR'
+            kind = f'No {srvc} output [expected={fl}]'
+            err_dct = {'cod': cod, 'type': type, 'kind': kind}
+            err_dct_lst.append(err_dct)
+            continue
+    return err_dct_lst
+
+
+def compile_output_fls(data_dir, out_dir, depth):
+    '''
+    Compile Viralflow output data files on subdirs
+    Parameters
+    ==========
+    data_dir:<path>
+        path for dir containing results output subdirs
+    out_dir:<path>
+        output dir to write compiled data
+    depth:<depth>
+        depth prefix used
+    '''
+    # create outdir if does not exist
+    try:
+        assert(os.path.exists(out_dir))
+    except(AssertionError):
+        print('@ creating output dir')
+        os.system('mkdir '+out_dir)
+
+    print('@ compiling output files')
+    # create multifasta file
+    multifas_fl = open(data_dir+'seqbatch.fa', 'w')
+
+    # create list to store data
+    pango_df_lst = []
+    chrms_df_lst = []
+    nxtcd_df_lst = []
+    ithst_df_lst = []
+    mut_df_lst = []
+    err_dct_lst = []
+    skip_lst = []
+
+    # iterate over subdirs on data dir
+    c = 0
+    for path, subdirs, files in os.walk(data_dir):
+        for name in files:
+            # check only files at *.results subdirs
+            sub_dir = path.split('/')[-1]
+            if sub_dir.endswith('.results') is False:
+                continue
+            # get consensus
+            cod = sub_dir.split('.')[0]
+            prfx = path+f'/{cod}.'
+            if cod in skip_lst:
+                continue
+            # look for consensus sequence fasta, if yes, then check for
+            # 1) expected files
+            # 2) parse and compiled results in single files
+            if name.endswith(f'depth{depth}.fa'):
+                c += 1
+                print(f'  > {c} samples processed', end='\r')
+                fasta_path = os.path.join(path, name)
+                # sanity
+                assert(cod in name)
+                # write sequence to new
+                seq = get_consensus_seq(fasta_path, cod)
+                if len(seq) == 1:
+                    type = 'ERROR'
+                    kind = 'No consensus sequence obtained'
+                    print(
+                      f'  :: WARNING: No consensus sequence obtained for {cod}'
+                    )
+                    err_dct_lst.append(
+                        {'cod': cod, 'type': type, 'kind': kind})
+                    skip_lst.append(cod)
+                    continue
+                # write to multifasta
+                multifas_fl.write('> '+cod+'\n')
+                multifas_fl.write(seq+'\n')
+                # continue
+                # -- SET EXPECTED OUTPUT FILES --------------------------------
+                # pangolin
+                try:
+                    pango_fl = f'{prfx}depth{depth}.fa.pango.csv'
+                    assert(os.path.isfile(pango_fl))
+                except(AssertionError):
+                    pango_fl = f'{prfx}depth{depth}.all.fa.pango.csv'
+                # chromosomes
+                chrms_fl = f'{path}/chromosomes.report'
+                # intrahost
+                ithst_fl = f'{prfx}depth{depth}.fa.bc.intrahost.tsv'
+                # mutation
+                mut_fl = f'{prfx}tsv'
+                # nextclade
+                try:
+                    nxtcl_fl = f'{prfx}depth{depth}.fa.nextclade.csv'
+                    assert(os.path.isfile(nxtcl_fl))
+                except(AssertionError):
+                    nxtcl_fl = f'{prfx}depth{depth}.all.fa.nextclade.csv'
+
+                # --- CHECK IF FILES EXIST ------------------------------------
+                out_fls_lst = [pango_fl, chrms_fl, ithst_fl, mut_fl]
+                err_dct_ = __check_if_outfls_exist(cod, out_fls_lst, mut_fl)
+                # if any file is missing, skip current sample
+                if len(err_dct_) > 0:
+                    for err in err_dct_:
+                        err_dct_lst.append(err)
+                    break
+
+                # --- PARSE DATA ----------------------------------------------
+                # pangolin
+                pango_df_lst.append(__parse_xsv(cod, pango_fl, sep=','))
+                # chromossomes
+                chrms_df_lst.append(__parse_xsv(cod, chrms_fl, sep='\t'))
+                # nextclade
+                nxtcd_df_lst.append(__parse_xsv(cod, nxtcl_fl, sep=';'))
+                # intrahost
+                ithst_df_lst.append(__parse_xsv(cod, ithst_fl, sep='\t'))
+
+                # Mutations
+                mut_df = pd.read_csv(mut_fl, sep='\t')
+                val_mut_df = mut_df.loc[mut_df['PASS'] == True]
+                mut_lst = val_mut_df.apply(get_mut, axis=1).values
+                dct = {'cod': cod, 'mut': mut_lst}
+                mut_df_lst.append(pd.DataFrame(dct))
+        # if name.endswith('intrahost.short.tsv')
+    print(f'  > Total {c} samples processed')
+
+    print('@ writing compiled data')
+    # mount compileds dataframes
+    all_pango_df = pd.concat(pango_df_lst, ignore_index=True)
+    all_nxtcd_df = pd.concat(nxtcd_df_lst, ignore_index=True)
+    all_chrms_df = pd.concat(chrms_df_lst, ignore_index=True)
+    all_mut_df = pd.concat(mut_df_lst, ignore_index=True)
+    errors_df = pd.DataFrame(err_dct_lst)
+    # write csvs
+    all_pango_df.to_csv(out_dir+'pango.csv')
+    print('  > pango.csv')
+    all_nxtcd_df.to_csv(out_dir+'nextclade.csv')
+    print('  > nextclade.csv')
+    all_chrms_df.to_csv(out_dir+'chromossomes.csv')
+    print('  > chromossomes.csv')
+    all_mut_df.to_csv(out_dir+'mutations.csv')
+    print('  > mutations.csv')
+    errors_df.to_csv(out_dir+'errors_detected.csv')
+    print('  > errors_detected.csv')
+    print(' :: DONE ::')
