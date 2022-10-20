9402c76cc9eddca4386de5700c701362da49ee56
Bruno Dotto <bdotto82@gmail.com>
2022-10-20 07:49:03 -0300

    Ajustes para rodar sem root

--- vfnext/containers/setupContainers.sh
+++ vfnext/containers/setupContainers.sh
@@ -15,58 +15,58 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 # -----------------------------------------------------------------------------
 
 echo "@ Building singularity_bam_readcount.sif..."
-singularity build singularity_bam_readcount.sif docker://mgibio/bam-readcount
+singularity build --fakeroot --sandbox singularity_bam_readcount.sif docker://mgibio/bam-readcount
 echo "  > Done <"
 
 echo "@ Building singularity_mafft.sif..."
-singularity build singularity_mafft.sif docker://staphb/mafft
+singularity build --fakeroot --sandbox singularity_mafft.sif docker://staphb/mafft
 echo "  > Done <"
 
 echo "@ Building singularity_bedtools.sif..."
-singularity build singularity_bedtools.sif docker://staphb/bedtools
+singularity build --fakeroot --sandbox singularity_bedtools.sif docker://staphb/bedtools
 echo "  > Done <"
 
-echo "@ Building nextclade_2.4.0.sif..."
-singularity pull docker://nextstrain/nextclade:2.4.0
-
 echo "  > loading sars-cov2 nextclade dataset..."
-./nextclade_2.4.0.sif nextclade dataset get --name 'sars-cov-2' --output-dir 'nextclade_dataset/sars-cov-2'
+singularity build --fakeroot --sandbox singularity_nextclade.sif Singularity_nextclade
+singularity exec -B nextclade_dataset/sars-cov-2:/tmp singularity_nextclade.sif nextclade dataset get --name 'sars-cov-2' --output-dir '/tmp'
 echo "  > Done <"
 
 echo "@ Building pangolin_latest.sif..."
-singularity pull docker://staphb/pangolin
+#singularity pull docker://staphb/pangolin
+#singularity build --fakeroot --sandbox my_sbox my_image.sif
+singularity build --fakeroot --sandbox pangolin_latest.sif Singularity_pangolin
 echo "  > Done <"
 
 echo "@ Building singularity_picard.sif..."
-singularity build --fakeroot singularity_picard.sif Singularity_picard
+singularity build --fakeroot --sandbox singularity_picard.sif Singularity_picard
 echo "  > Done <"
 
 echo "@ Building singularity_bwa_v0717.sif..."
-singularity build --fakeroot singularity_bwa_v0717.sif Singularity_bwa_v0717
+singularity build --fakeroot --sandbox singularity_bwa_v0717.sif Singularity_bwa_v0717
 echo "  > Done <"
 
 echo "@ Building singularity_fastp_v0201.sif..."
-singularity build --fakeroot singularity_fastp_v0201.sif Singularity_fastp_v0201
+singularity build --fakeroot --sandbox singularity_fastp_v0201.sif Singularity_fastp_v0201
 echo "  > Done <"
 
 echo "@ Building singularity_bwa_v0717.sif..."
-singularity build --fakeroot ivar_latest.sif Singularity_ivar
+singularity build --fakeroot --sandbox ivar_latest.sif Singularity_ivar
 echo "  > Done <"
 
 echo "@ Building singularity_pythonScripts.sif..."
-singularity build --fakeroot singularity_pythonScripts.sif Singularity_pythonScripts
+singularity build --fakeroot --sandbox singularity_pythonScripts.sif Singularity_pythonScripts
 
 echo "@ Building singularity_edirect.sif..."
-singularity build --fakeroot singularity_edirect.sif Singularity_edirect
+singularity build --fakeroot --sandbox singularity_edirect.sif Singularity_edirect
 
 echo "@ Building singularity_snpeff.sif..."
 singularity build --fakeroot --sandbox singularity_snpeff.sif Singularity_snpEff
 
 echo "@ Building singularity_samtools.sif..."
-singularity build --fakeroot singularity_samtools.sif docker://staphb/samtools:1.15
+singularity build --fakeroot --sandbox singularity_samtools.sif docker://staphb/samtools:1.15
 
 echo "@ Building vfreport.sif..."
-singularity build --fakeroot vfreport.sif Singularity_vfreport
+singularity build --fakeroot --sandbox vfreport.sif Singularity_vfreport
 
 echo "@ Downloading snpeff database catalog..."
 singularity exec ./singularity_snpeff.sif snpEff databases > snpEff_DB.catalog
--- wrapper/install_dep.sh
+++ wrapper/install_dep.sh
@@ -1,13 +1,20 @@
 # install miniconda
 echo "@ Install Miniconda..."
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
-bash Miniconda3-py39_4.12.0-Linux-x86_64.sh
+bash Miniconda3-py39_4.12.0-Linux-x86_64.sh -b
+
+echo "@ Config Miniconda..."
+$HOME/miniconda3/bin/conda init
+source $HOME/.bashrc
+conda update conda -y
+conda create -n viralflow python=3.9 -y
+conda activate viralflow
 
 # install singularity 
 echo "@ install singularity"
-conda install -c "conda-forge/label/main" singularity
+conda install -c "conda-forge/label/main" singularity -y
 
 # install nextflow 
 echo "@ install nextflow"
-conda install -c bioconda nextflow
+conda install -c bioconda nextflow -y
 nextflow self-update
