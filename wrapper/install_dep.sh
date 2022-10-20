49ceab8f4347250f7c4eefc7791980e223c9d0c5
Bruno Dotto <bdotto82@gmail.com>
2022-10-20 08:21:01 -0300

    Ajustes setup

--- wrapper/install_dep.sh
+++ wrapper/install_dep.sh
@@ -5,7 +5,7 @@ bash Miniconda3-py39_4.12.0-Linux-x86_64.sh -b
 
 echo "@ Config Miniconda..."
 $HOME/miniconda3/bin/conda init
-exec bash
+source $HOME/.bashrc
 $HOME/miniconda3/bin/conda update conda -y
 $HOME/miniconda3/bin/conda create -n viralflow python=3.9 -y
 $HOME/miniconda3/bin/conda activate viralflow
