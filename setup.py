f691987a9fab5079bd783e9974f5d53e26421007
AMarinhoSN <ad45@sanger.ac.uk>
2023-09-06 21:00:22 +0100

    update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ViralFlow',
-      version='1.0.0',
+      version='0.1.0',
       description='''
       Workflows for viral genome Assembly at FioCruz/IAM
       ''',
