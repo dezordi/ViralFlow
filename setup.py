0499fe24c8ec17d8df70b37defb025ea9ff7f612
dezordi <zimmer.filipe@gmail.com>
2023-10-14 16:31:09 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 
 setup(name='ViralFlow',
-      version='0.1.0',
+      version='0.2.0',
       description='''
       Workflows for viral genome Assembly at FioCruz/IAM
       ''',
       url='https://github.com/dezordi/ViralFlow/',
       author='Antonio Marinho & Filipe Z. Dezordi',
       author_email='amarinhosn@pm.me & zimmer.filipe@gmail.com',
-      #packages=['viralflow'],
       py_modules = [],
       classifiers=[
         'Programming Language :: Python :: 3.8',
