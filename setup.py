0422cef6e105aaaae6b2a2d0f569d1387892cdfc
alexandrefreitasdasilva <alexfreitasbiotec@gmail.com>
2023-11-11 16:50:25 -0300

    changing the version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ViralFlow',
-      version='0.1.0',
+      version='0.1.1',
       description='''
       Workflows for viral genome Assembly at FioCruz/IAM
       ''',
